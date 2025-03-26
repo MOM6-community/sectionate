ff9a0590c0e69a70bfda540bbed9756e77a60b8a
Henri Drake <henrifdrake@gmail.com>
2025-03-25 21:11:35 -0400

    Reworked all notebooks to use output downloaded from Zenodo

---
+++ data/README.md
@@ -0,0 +1,1 @@
+# Data for examples notebooks and testing
---
+++ examples/load_example_model_grid.py
@@ -0,0 +1,29 @@
+import urllib.request
+import shutil
+import os
+import xarray as xr
+import xgcm
+
+def download_MOM6_example_data():
+    # download the data
+    url = 'https://zenodo.org/record/15085089/files/'
+    file_name = 'MOM6_global_example_diagnostics.nc'
+    destination_path = f"../data/{file_name}"
+    if not os.path.exists(destination_path):
+        with urllib.request.urlopen(url + file_name) as response, open(destination_path, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+        print(f"File '{file_name}' has been downloaded.")
+    else:
+        print(f"File '{file_name}' already exists. Skipping download.")
+
+    return destination_path
+
+def load_MOM6_example_grid():
+    destination_path = download_MOM6_example_data()
+    ds = xr.open_dataset(destination_path)
+    coords={
+        'X': {'center': 'xh', 'outer': 'xq'},
+        'Y': {'center': 'yh', 'outer': 'yq'},
+    }
+    grid = xgcm.Grid(ds, coords=coords, boundary={"X":"periodic", "Y":"extend"}, autoparse_metadata=False)
+    return grid
