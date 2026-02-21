5523a82f807c4801adfc04d5728f50b5b44cb35d
Henri Drake <hfdrake@uci.edu>
2026-02-20 20:46:51 -0800

    Update conf.py for sectionate

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ master_doc = "index"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 
-project = 'xbudget'
+project = 'sectionate'
 copyright = '2026, Henri Drake'
 author = 'Henri Drake'
 
